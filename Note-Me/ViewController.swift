d157f4e118ddd9f055048f941f4ec2551c720460
Shanmugarajeshwaran R <tweets.shan@gmail.com>
2021-07-03 18:58:14 +0530

    Initial Commit

---
+++ Note-Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Note-Me
+//
+//  Created by ADMIN on 03/07/21.
+//  Copyright © 2021 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
